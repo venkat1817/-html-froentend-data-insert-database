f987d2e0223fa16d01176883ec79f7afc38d5400
venkat <118958583+venkat1817@users.noreply.github.com>
2023-05-25 13:11:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
+#same directory crete two files 1.app.py , 2.db.py
+import mysql.connector
+
+# Establish connection to the MySQL database
+cnx = mysql.connector.connect(
+    host="database-reddy.cdcacngfasur.us-east-1.rds.amazonaws.com",
+    user="admin",
+    password="lkjhgfdsa",
+    database="application_db"
+)
+
+def create_table():
+    cursor = cnx.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL,
+            message TEXT,
+            gender VARCHAR(10),
+            interests VARCHAR(255)
+        )
+    """)
+    cnx.commit()
+
+       
 
